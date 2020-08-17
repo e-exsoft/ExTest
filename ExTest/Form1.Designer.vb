66453b9c1fd89ae3de96544740721ae9686a8905
Ersin KOCABAŞLI <exsoft@msn.com>
2020-08-17 13:08:37 +0300

    Proje dosyası ekle.

---
+++ ExTest/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(109, 47)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+End Class
---
+++ ExTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+' denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+' bu öznitelik değerlerini değiştirin.
+
+' Bütünleştirilmiş kod özniteliklerinin değerlerini gözden geçirin
+
+<Assembly: AssemblyTitle("ExTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ExTest")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Bu proje COM'un kullanımına sunulursa aşağıdaki GUID, typelib kimliği içindir
+<Assembly: Guid("ee0030e3-790b-48ce-ad29-c2ccd45c6eb0")>
+
+' Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+'
+'      Ana Sürüm
+'      İkincil Sürüm 
+'      Yapı Numarası
+'      Düzeltme
+'
+' Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+' aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
